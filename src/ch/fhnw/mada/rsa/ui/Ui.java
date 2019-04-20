828bd0f5192eb807e64ca5656b3f55019d4d3c0b
JungEdi <eduard.jung@hotmail.com>
2019-04-20 15:30:50 +0200

    Update Ui.java

--- src/ch/fhnw/mada/rsa/ui/Ui.java
+++ src/ch/fhnw/mada/rsa/ui/Ui.java
@@ -1,8 +1,18 @@
 package ch.fhnw.mada.rsa.ui;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.math.BigInteger;
 import java.util.Scanner;
+import java.util.stream.Stream;
+
+import ch.fhnw.mada.rsa.TokenPair;
+import ch.fhnw.mada.rsa.Translator;
+import ch.fhnw.mada.rsa.utils.Tuple;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -15,6 +25,11 @@ import javafx.stage.FileChooser;
 import javafx.stage.Stage; 
 
 public class Ui extends VBox {
+	private Integer rsaN;
+	private Integer rsaD;
+	private Integer rsaE;
+	
+	private TokenPair tokenPair;
 	private Text title;
 	private StackPane titleLayout;
 	private TextField path;
@@ -24,6 +39,8 @@ public class Ui extends VBox {
 	private Stage stage;
     private FileChooser fileChooser;
     private GridPane generate;
+    private String message;
+    private Scanner msg;
 
 	private GridPane publicKeyPane;
 	private GridPane privateKeyPane;
@@ -34,16 +51,9 @@ public class Ui extends VBox {
 	private Button generateKeyPair;
     private Scanner sPub;
     private Scanner sPri;
-    /*private HBox showN;
-    private HBox showD;
-    private HBox showE;
-    private Label n;
-    private Label d;
-    private Label e;
-    private Label LabelN;
-    private Label LabelE;
-    private Label LabelD;
-    private VBox showrsaKeys;*/
+    
+    private Button encrypt;
+    private Button decrypt;
     
 	private TextField pathPri;
 	private TextField pathPub;
@@ -58,8 +68,25 @@ public class Ui extends VBox {
 		//Choose the TextFile
 		fileChooser = new FileChooser();
 		folderChooser = new GridPane();
-		selector = new Button("Select Folder");
-		selector.setOnAction(x -> pm.path.set(fileChooser.showOpenDialog(stage).getPath()));
+		selector = new Button("Select File");
+		selector.setOnAction(x -> {
+			try {
+        		//File erstellen
+        		File f = new File("");
+        		f = fileChooser.showOpenDialog(stage);
+        		//File lesen
+        		msg = new Scanner(f);
+        		//message lesen
+    			message = new String(sPub.next());
+    			
+    			//Pfad binden
+    			pm.path.set(f.getPath());       			
+            	
+			} catch (FileNotFoundException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}	
+		});
 		
 		//Show Textfile Path
 		path = new TextField();
@@ -118,13 +145,6 @@ public class Ui extends VBox {
 					}		
 		});
 		
-		//Show Keys
-		/*LabelN = new Label();
-		LabelE = new Label();
-		LabelD = new Label();
-		LabelN.textProperty().bind(pm.rsaN);
-		LabelE.textProperty().bind(pm.rsaE);
-		LabelD.textProperty().bind(pm.rsaD);*/
 		pathPri = new TextField();
 		pathPri.textProperty().bind(pm.pathPri);
 		pathPub = new TextField();
@@ -132,24 +152,74 @@ public class Ui extends VBox {
 		
 		
 		//Title
-		title = new Text("RSA Assesment");
+		title = new Text("RSA Assesment"); 
 		titleLayout= new StackPane();
 		
+		//Generiert Public und Private Key und speichert diese in sk.txt(private key) und pk.txt(public key) mit , getrennt
 		generate = new GridPane();
 		generateKeyPair = new Button("Generate Key Pair");
-		//Action to Generate a Pair selector.setOnAction();
+		generateKeyPair.setOnAction(x -> {
+			tokenPair = TokenPair.createToken(2048);
+			
+			PrintWriter pkWriter = null; 
+			PrintWriter skWriter = null; 
+	        try { 
+	        	pm.rsaN.set(tokenPair.privateKey().v1.toString());
+	        	pm.rsaD.set(tokenPair.privateKey().v2.toString());
+	        	pm.rsaE.set(tokenPair.publicKey().v2.toString());
+	        	skWriter = new PrintWriter(new BufferedWriter(new FileWriter("sk.txt"))); 
+	        	skWriter.println("(" + tokenPair.privateKey().v1 + "," + tokenPair.privateKey().v2 + ")");
+	        	pkWriter = new PrintWriter(new BufferedWriter(new FileWriter("pk.txt"))); 
+	        	pkWriter.println("(" + tokenPair.publicKey().v1 + "," + tokenPair.publicKey().v2 + ")"); 
+	        } catch (IOException ioe) { 
+	            ioe.printStackTrace(); 
+	        } finally { 
+	            if (skWriter != null){ 
+	            	skWriter.flush(); 
+	            	skWriter.close(); 
+	            } 
+	            if (pkWriter != null){ 
+	            	pkWriter.flush(); 
+	            	pkWriter.close(); 
+	            } 
+	        } 
+			
+		});
 		
-		/*showN = new HBox();
-		showE = new HBox();
-		showD = new HBox();
-		showrsaKeys = new VBox();
+		encrypt = new Button("Encrypt");
+		encrypt.setOnAction(x -> {
+			
+			var translator = new Translator( 
+					new Tuple<BigInteger>(pm.rsaN, pm.rsaD),
+					new Tuple<BigInteger>(pm.rsaN, pm.rsaE)
+			);
+			
+			   PrintWriter encryptMsg = new PrintWriter(new BufferedWriter(new FileWriter("encryptedMsg.txt"))); 
+			   for (int i = 0; i < message.length(); i++){
+				    char c = message.charAt(i);     
+				    int ascii = (int) c;
+				    encryptMsg.print(translator.encryptMessage(BigInteger.valueOf(ascii)));
+				}
+			
+			
+		});
 		
-		 n = new Label("N:");
-		 e = new Label("E:");
-		 d = new Label("D:");*/
-		
-		
-		
+		encrypt = new Button("Decrypt");
+		encrypt.setOnAction(x -> {
+
+			var translator = new Translator( 
+					new Tuple<BigInteger>(pm.rsaN, pm.rsaD),
+					new Tuple<BigInteger>(pm.rsaN, pm.rsaE)
+			);
+			
+			   PrintWriter decryptMsg = new PrintWriter(new BufferedWriter(new FileWriter("decryptedMsg.txt"))); 
+			   for (int i = 0; i < message.length(); i++){
+				    char c = message.charAt(i);     
+				    int ascii = (int) c;
+				    decryptMsg.print(translator.decryptMessage(BigInteger.valueOf(ascii)));
+				}
+			
+		});
 		
 		
 	}
